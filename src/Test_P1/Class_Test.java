db69d6cd4e66bf4217cc176222f5f948badf9f24
DATTARAJ WETE <dattarajwete@gmail.com>
2021-09-01 11:43:24 +0530

    conflict resolved

--- src/Test_P1/Class_Test.java
+++ src/Test_P1/Class_Test.java
@@ -4,5 +4,6 @@ public class Class_Test {
 	public static void main(String[] args) {
 		System.out.println("Helloo");
 		System.out.println("Branch A");
+		System.out.println("Branch A & B conflict resolved");
 	}
 }
